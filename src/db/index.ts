57ea49e42c9af806cb6577899ac81cebf22f4b84
Lucky <lucky123@gmail.com>
2024-10-19 21:00:57 +0530

    setup

---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+// Make sure to install the 'postgres' package
+import { drizzle } from 'drizzle-orm/postgres-js';
+import * as schema from "./schema"
+import postgres from 'postgres';
+
+const queryClient = postgres(process.env.DATABASE_URL!);
+const db = drizzle( queryClient,{schema} );
+
+export {db}
